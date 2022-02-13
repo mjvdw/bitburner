ecc5660d4c3e5c140b3fdb0648d985bdf4c3b73a
Michael van de Water <me@mjvdw.com>
2022-02-13 18:03:40 +1300

    Initial set up for status helper

--- scripts/999-status.js
+++ scripts/999-status.js
@@ -1,2 +1,7 @@
 /** @param {import(".").NS} ns **/
-export async function main(ns) {}
+export async function main(ns) {
+  let server = ns.args[0] || "home";
+  let target = ns.args[1] || undefined;
+
+  let scripts = ns.ps(server);
+}
