c02eb1ef50044b995c93ec149709e9b0bfbb9e9f
Michael van de Water <me@mjvdw.com>
2022-02-23 20:40:55 +1300

    Rewriting getAllServers function

--- scripts/src/lazarus.ts
+++ scripts/src/lazarus.ts
@@ -1,8 +1,18 @@
 /** @param {import(".").NS} ns */
 
 // @ts-ignore
-import * as bu from "/scripts/utils.js";
+import { getAllServers } from "/scripts/utils.js";
 
+/**
+ * This is the "master" script for hacking servers.
+ * It controls:
+ * - Deciding which server to hack.
+ * - Purchasing new client servers to increase hacking power.
+ * - Starting batch controllers for each server.
+ * 
+ * @param ns Netscript object provider by Bitburner
+ */
 export async function main(ns: any) {
-    bu.somethingAwesome(ns = ns);
+    let servers = getAllServers(ns)
+    ns.tprint(servers)
 }
--- scripts/src/utils.ts
+++ scripts/src/utils.ts
@@ -6,4 +6,28 @@
  */
 export function getAllServers(ns: any) {
 
+    let servers: string[] = []
+    let queue: string[] = []
+    let excluded = ["home", "darkweb"]
+
+    // Initial scan to set up first queue.
+    queue = ns.scan("home")
+
+    // Iterate through initial queue, adding new servers as it finds them.
+    // By ignoring duplicates, this is not an infinite loop.
+    while (queue.length > 0) {
+        let scanQueue = queue
+        for (let s in scanQueue) {
+            servers.push(scanQueue[s])
+            queue = queue.filter(value => value !== scanQueue[s])
+            let newServers = ns.scan(scanQueue[s])
+            newServers.forEach((newServer: string) => {
+                if (excluded.includes(newServer) == false && servers.includes(newServer) == false) {
+                    queue.push(newServer)
+                }
+            })
+        }
+    }
+
+    return servers
 }
