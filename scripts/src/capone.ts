d84d6b72c8288f1ffcc7f3144d975d6bf6b124ca
Michael van de Water <me@mjvdw.com>
2022-03-12 14:34:11 +1300

    Improve crime loop

--- scripts/src/capone.ts
+++ scripts/src/capone.ts
@@ -4,16 +4,21 @@
 import { } from "/scripts/utils.js";
 
 /**
- * 
+ * Simple loop designed to commit crimes on repeat. Initially
+ * this is the fastest way to make money, but is only available
+ * with the Singlularity BitNode (BN-4).
  * 
  * @param ns Netscript object provider by Bitburner
  */
 export async function main(ns: any) {
 
+    let crime = ns.args[0] || "shoplift"
+
     while (true) {
+        let crimeTime = 0
         if (!ns.isBusy()) {
-            await ns.commitCrime("mug someone")
+            crimeTime = ns.commitCrime(crime)
         }
-        await ns.sleep(5000)
+        await ns.sleep(crimeTime + 2000)
     }
 }
