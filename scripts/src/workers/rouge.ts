05875da647305bd3756f73d33e73d5e857ef3740
Michael van de Water <me@mjvdw.com>
2022-03-20 12:37:51 +1300

    Initial set up for Rouge

---
+++ scripts/src/workers/rouge.ts
@@ -0,0 +1,14 @@
+/** @param {import(".").NS} ns */
+
+import {
+    // @ts-ignore
+} from "/scripts/library/utils.js";
+
+/**
+ * 
+ * 
+ * @param ns Netscript object provider by Bitburner
+ */
+export async function main(ns: any) {
+}
+
