b8da59b5dd6393f78ab63ab6575a610309fa4157
Michael van de Water <me@mjvdw.com>
2024-10-12 10:52:44 +1300

    Basic hacknet node script

--- scripts/src/workers/hacknet.ts
+++ scripts/src/workers/hacknet.ts
@@ -1,1 +1,20 @@
 /** @param {import(".").NS} ns */
+
+/**
+ * Automate the process of leveling up Hacknet Nodes.
+ * 
+ * @param ns Netscript object provider by Bitburner
+ */
+export async function main(ns: any) {
+    while (true) {
+        let numNodes = ns.hacknet.numNodes();
+        for (let i = 0; i < numNodes; i++) {
+            let cost = ns.hacknet.getLevelUpgradeCost(i);
+            let money = ns.getServerMoneyAvailable("home");
+            if (money > cost) {
+                ns.hacknet.upgradeLevel(i, 1);
+            }
+        }
+        await ns.sleep(1000);
+    }
+}
